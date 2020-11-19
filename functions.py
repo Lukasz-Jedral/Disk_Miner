038cb062e8de2d7188d2848f5ef0c1fec9de1a01
Lukasz Jedral <geodezja.jedral@gmail.com>
2020-11-19 12:44:43 +0100

    ADD function for choosing from given list only unique values

--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import sys
 from tkinter import filedialog, messagebox
 
+
 def get_file_path():
     """ Function will return selected file path.
         If no file selected warning message will pop up and program execution will be terminated
@@ -12,9 +13,17 @@ def get_file_path():
     file_path = filedialog.askopenfilename()
 
     if file_path == '':
-        messagebox.showwarning( title="Warning",
-                                message="No file was selected")
+        messagebox.showwarning(title="Warning",
+                               message="No file was selected")
 
         sys.exit(0)
 
     return file_path
+
+
+def unique_values_from_list(mylist):
+    """function returns only unique values form given list"""
+
+    list_set = set(mylist)  # insert the list to the set
+    unique_list = (list(list_set))  # convert the set to the list
+    return unique_list
